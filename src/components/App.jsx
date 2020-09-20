36a3209f9b860174a95c6d7fa0085845926c05d2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-09-20 16:43:37 +0100

    adding file submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,20 +21,12 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                
                   <img src={logo} className="App-logo" alt="logo" />
-                </a>
+                
                 <h1>Welcome to Planet Git!</h1>
                 
                 Upload your git repo to IPFS & Ethereum! <br /><br />
-                <form>
-                    <input type="file" id="filepicker1" name="fileList" webkitdirectory="true" multiple />
-                    <input type='submit' />
-                </form>
               <DirectoryListing />
               </div>
               
--- src/components/directoryListing.jsx
+++ src/components/directoryListing.jsx
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class DirectoryListing extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          buffer: null
+      };
+    }
+
+    captureFile = (event) => {
+    event.preventDefault();
+    console.log('The file has been captured!');
+    const file = event.target.files[0];
+    console.log('This is the upload: ', file);
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+        console.log('load end...');
+        console.log('Buffer: ', Buffer(reader.result));
+    }
+  }
+
+  onSubmit = (event) => {
+  event.preventDefault();
+  console.log('The file has been Submitted!');
+  
+}
+
   render() {
     return (
       <div>
-        
+      <form onSubmit={this.onSubmit} >
+          {/* <input type="file" id="filepicker" name="fileList" webkitdirectory="true" multiple /> */}
+          <input type="file" id="filepicker" name="fileList" onChange={this.captureFile}/>
+          <input type='submit'  />
+      </form>
         <ul id="listing"></ul>
       </div>
     );
@@ -14,17 +44,19 @@ class DirectoryListing extends Component {
 
 export default DirectoryListing;
 
-
-const fileUpload = document.getElementById("filepicker")
-if(fileUpload){
-    fileUpload.addEventListener("change", function(event) {
-        let output = document.getElementById("listing");
-        let files = event.target.files;
+// function showList(event){
+//     console.log('fileUpload 1');
+//     console.log('fileUpload 2');
+//     let output = document.getElementById("listing");
+//     console.log('fileUpload 3');
+//     let files = event.target.files;
+//     console.log('fileUpload 4');
+    
+//     for (let i=0; i<files.length; i++) {
+//         console.log('fileUpload 5');
+//         let item = document.createElement("li");
+//         item.innerHTML = files[i].webkitRelativePath;
+//         output.appendChild(item);
+//     };
       
-        for (let i=0; i<files.length; i++) {
-          let item = document.createElement("li");
-          item.innerHTML = files[i].webkitRelativePath;
-          output.appendChild(item);
-        };
-      }, false);
-}
+//   }
