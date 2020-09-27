d7c99a27ed38b82a1c0e327b864c6f05a8d2c573
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-09-27 19:47:53 +0300

    adding repo to smart contract

--- src/contracts/data.sol
+++ src/contracts/data.sol
@@ -2,16 +2,26 @@ pragma solidity >=0.7.0;
 
 contract Data {
     string projectName;
+    string repoHash;
     string imgHash = 'QmNWxPVpr26ichSV9jBdPrFdjPTXBx5f1XQG4roZtVNrah';
 
-    // Write function
+    // Write Project Name function
     function setName(string memory _projectName) public{
         projectName = _projectName;
     }
-    // Write function
-    function setAll(string memory _projectName, string memory _imgHash) public{
+    // Write REPO function
+    function setImg(string memory _imgHash) public{
+        imgHash = _imgHash;
+    }
+    // Write REPO function
+    function setRepo(string memory _repoHash) public{
+        repoHash = _repoHash;
+    }
+    // Write ALL function
+    function setAll(string memory _projectName, string memory _imgHash, string memory _repoHash) public{
         projectName = _projectName;
         imgHash = _imgHash;
+        repoHash = _repoHash;
     }
     // Read function
     function getName() public view returns(string memory) {
@@ -20,5 +30,9 @@ contract Data {
     // Read function
     function getImg() public view returns(string memory) {
         return imgHash;
+    }
+    // Read function
+    function getRepo() public view returns(string memory) {
+        return repoHash;
     }
 }
--- test/data.test.js
+++ test/data.test.js
@@ -34,14 +34,35 @@ contract('Data', (accounts)=>{
             const result = await data.getName();
             assert.equal(result, projectName);
         })
+        it('Updates project Name', async () => {
+            let projectName = 'abc123';
+            await data.setName(projectName);
+            const resultData = await data.getName();
+            assert.equal(resultData, projectName);
+        })
+        it('Updates Image Hash', async () => {
+            let imgHash = 'abc12321092190';
+            await data.setImg(imgHash);
+            const resultImg = await data.getImg();
+            assert.equal(resultImg, imgHash);
+        })
+        it('Updates Repo Hash', async () => {
+            let repoHash = 'lksdjkdopewabc12321092190';
+            await data.setRepo(repoHash);
+            const resultRepo = await data.getRepo();
+            assert.equal(resultRepo, repoHash);
+        })
         it('Updates Everything', async () => {
             let projectName = 'abc123';
             let imgHash = 'abc12321092190';
-            await data.setAll(projectName, imgHash);
+            let repoHash = 'lksdjkdopewabc12321092190';
+            await data.setAll(projectName, imgHash, repoHash);
             const resultImg = await data.getImg();
             const resultData = await data.getName();
+            const resultRepo = await data.getRepo();
             assert.equal(resultData, projectName);
             assert.equal(resultImg, imgHash);
+            assert.equal(resultRepo, repoHash);
         })
     })
 })
