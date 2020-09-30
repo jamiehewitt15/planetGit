39c9e1d995088f28fc1a1a4f0c8a49476ca1cc12
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-09-30 20:05:55 +0300

    adding git pull

--- cli/pull.js
+++ cli/pull.js
@@ -17,7 +17,12 @@ if (!repoHash){
 
 const cloneRepo = async() => {
   console.log('Cloning into: ', 'http://127.0.0.1:8080/ipfs/' + repoHash);
-  shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ${newFolder}`);
+  if (shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ../${newFolder}`).code !== 0) {
+    console.log("Waiting for IPFS daemon to start...")
+    await sleep(4000);
+    shell.echo('Trying again...');
+    shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ../${newFolder}`)
+  }
   console.log("Finished")
 }
 
@@ -32,14 +37,8 @@ const startClone = async() => {
   } else{
     if (shell.which('ipfs')) { // check if user has IPFS installed
       shell.exec(`ipfs daemon`, {async:true, silent:true});
-      try{
-        await sleep(2000);
-        await cloneRepo();
-      } catch(e){
-        console.log("Waiting for IPFS daemon to start...")
-        await sleep(4000);
-        await cloneRepo();
-      }
+      await sleep(2000); // wait for IPFS Daemon to start
+      await cloneRepo();
       }
       else{ // Use public IPFS node
       console.log("We recomend installing IPFS")
