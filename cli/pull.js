f63d29581b462d9cc64af112bb79a8bc08ea725d
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-09-30 20:44:03 +0300

    changes to pull

--- cli/pull.js
+++ cli/pull.js
@@ -8,20 +8,17 @@ let repoHash = argv._[1];
 console.log('newFolder', newFolder);
 console.log('repoHash', repoHash);
 // Ask for user input
-if (!newFolder){
-  repoHash = readlineSync.question('Please enter the new folder name: ');
-}
 if (!repoHash){
   repoHash = readlineSync.question('Please enter the Hash of the repository you would like to clone: ');
 }
 
-const cloneRepo = async() => {
+const pullRepo = async() => {
   console.log('Cloning into: ', 'http://127.0.0.1:8080/ipfs/' + repoHash);
-  if (shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ../${newFolder}`).code !== 0) {
+  if (shell.exec(`git pull http://127.0.0.1:8080/ipfs/${repoHash}`).code !== 0) {
     console.log("Waiting for IPFS daemon to start...")
     await sleep(4000);
     shell.echo('Trying again...');
-    shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ../${newFolder}`)
+    shell.exec(`git pull http://127.0.0.1:8080/ipfs/${repoHash}`)
   }
   console.log("Finished")
 }
@@ -30,7 +27,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const startClone = async() => {
+const startPull = async() => {
   if (!shell.which('git')) { // check if user has git installed
     shell.echo('Sorry, this script requires git');
     shell.exit(1);
@@ -38,7 +35,7 @@ const startClone = async() => {
     if (shell.which('ipfs')) { // check if user has IPFS installed
       shell.exec(`ipfs daemon`, {async:true, silent:true});
       await sleep(2000); // wait for IPFS Daemon to start
-      await cloneRepo();
+      await pullRepo();
       }
       else{ // Use public IPFS node
       console.log("We recomend installing IPFS")
@@ -48,7 +45,7 @@ const startClone = async() => {
   }
 }
 
-startClone();
+startPull();
 
 
 
