d313f4b0a122dfeff8e473173523adf3df22ef0a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-09-30 13:50:27 +0300

    adding promises to daemon function

--- cli/clone.js
+++ cli/clone.js
@@ -1,17 +1,61 @@
 const shell = require('shelljs');
 const readlineSync = require('readline-sync');
+const ipfsClient = require('ipfs-http-client');
+const swarm = require('ipfs-http-client/src/swarm');
+const argv = require('minimist')(process.argv.slice(2));
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' });
+
+
+
+// Assign arguments
+let newFolder = argv._[0];
+let repoHash = argv._[1];
+console.log('newFolder', newFolder);
+console.log('repoHash', repoHash);
 
 // Wait for user's response.
-var userName = readlineSync.question('Please enter the Hash of the repository you would like to clone: ');
-console.log('cloning into: ', 'http://127.0.0.1:8080/ipfs/' + userName);
+if (!newFolder){
+  repoHash = readlineSync.question('Please enter the new folder name: ');
+}
+if (!repoHash){
+  repoHash = readlineSync.question('Please enter the Hash of the repository you would like to clone: ');
+}
+
+console.log('1. cloning into: ', 'http://127.0.0.1:8080/ipfs/' + repoHash);
+console.log('Into folder:', newFolder);
 
 if (!shell.which('git')) { // check if user has git installed
   shell.echo('Sorry, this script requires git');
   shell.exit(1);
 } else{
-  shell.exec(`git clone http://127.0.0.1:8080/ipfs/${userName} testClone`)
+  if (shell.which('ipfs')) { // check if user has IPFS installed
+    startIPFS().then(cloneRepo())
+    }
+    else{ // Use public IPFS node
+    console.log("We recomend installing IPFS")
+    console.log("Attempting: requesting repo through INFURA public node")
+    shell.exec(`git clone https://${repoHash}.ipfs.infura-ipfs.io/ ${newFolder}`);
+  }
 }
 
+async function startIPFS(){
+  let ready = shell.exec('ipfs swarm addrs').code;
+  console.log("ready", ready)
+  while (ready !== 1){ // wait for daemon to start
+    shell.exec(`ipfs daemon`, {async:true, silent:true});
+    ready = shell.exec('ipfs swarm addrs', {async:true, silent:true}).code;
+    console.log("ready", ready);
+    if(ready === 1){
+      return true
+    }
+  }
+}
+function cloneRepo(){
+  console.log('2. cloning into: ', 'http://127.0.0.1:8080/ipfs/' + repoHash);
+  shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ${newFolder}`, {async:true});
+  console.log("Finished")
+  shell.exit(1);
+}
 
 
 
