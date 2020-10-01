e3684bb9e3ad66c919e1086c0c044e830219a421
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-10-01 09:28:37 +0300

    checking if username is unique in smart contract

--- src/contracts/data.sol
+++ src/contracts/data.sol
@@ -14,11 +14,11 @@ contract Data {
     }
 
     // mapping address to User 
-    mapping (address => User) users;
-    address[] public allUsers;
+    mapping (address => User) UserAddrsMap;
+    address[] public allUserAddrs;
     // mapping address to User 
-    mapping (bytes15 => Address) userAddress;
-    bytes15[] public usersAddresses;
+    mapping (bytes15 => User) userNameMap;
+    bytes15[] public allUserNames;
     // mapping address to User to Repo
     mapping (bytes15 => Repo) repos;
     // mapping address to User to Repo
@@ -26,16 +26,17 @@ contract Data {
     bytes15[] public allRepos;
     
     // Check Username is unique
-    function uniqueUsername(address member) public view returns(bool isUser) {
-        return (members[member] != 0);
+    function uniqueUsername(bytes15 _userName) public view returns(bool isUnique) {
+        return (allUserNames[_userName] == 0);
     }
     // Create User
     function createUser(bytes15 _userName) public{
-        if(users[msg.sender] =){
-
+        if(uniqueUsername(_userName)){
+        UserAddrsMap[msg.sender] = User(msg.sender, _userName);
+        userNameMap[_userName] = User(msg.sender, _userName);
+        allUserAddrs.push(msg.sender);
+        allUserNames.push(_userName);
         }
-        users[msg.sender] = User(msg.sender, _userName);
-        allUsers.push(msg.sender);
     }
     // Create Repo
     function createRepo(bytes15 _projectSlug, bytes15 _projectName, bytes memory _imgHash, bytes memory _repoHash) public{
