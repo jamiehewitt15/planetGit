201b59058835ec7679596b7ac8251c6a55b3e1f8
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-10-01 09:01:15 +0300

    creating additional user mapping

--- src/contracts/data.sol
+++ src/contracts/data.sol
@@ -1,56 +1,65 @@
 pragma solidity >=0.7.0;
-// pragma experimental ABIEncoderV2;
 
 contract Data {
     
     struct User {
         address _address;
-        string userName;
+        bytes15 userName;
     }
 
     struct Repo {
-        string repoName;
-        string imgHash;
-        string repoHash;
+        bytes15 repoName;
+        bytes imgHash;
+        bytes repoHash;
     }
 
     // mapping address to User 
     mapping (address => User) users;
     address[] public allUsers;
+    // mapping address to User 
+    mapping (bytes15 => Address) userAddress;
+    bytes15[] public usersAddresses;
     // mapping address to User to Repo
-    mapping (string => Repo) repos;
+    mapping (bytes15 => Repo) repos;
     // mapping address to User to Repo
-    mapping (address => mapping (string => Repo)) userRepos;
-    string[] public allRepos;
-
+    mapping (address => mapping (bytes15 => Repo)) userRepos;
+    bytes15[] public allRepos;
+    
+    // Check Username is unique
+    function uniqueUsername(address member) public view returns(bool isUser) {
+        return (members[member] != 0);
+    }
     // Create User
-    function createUser(string memory _userName) public{
+    function createUser(bytes15 _userName) public{
+        if(users[msg.sender] =){
+
+        }
         users[msg.sender] = User(msg.sender, _userName);
         allUsers.push(msg.sender);
     }
     // Create Repo
-    function createRepo(string memory _projectSlug, string memory _projectName, string memory _imgHash, string memory _repoHash) public{
+    function createRepo(bytes15 _projectSlug, bytes15 _projectName, bytes memory _imgHash, bytes memory _repoHash) public{
         userRepos[msg.sender][_projectSlug] = Repo(_projectName, _imgHash, _repoHash);
         allRepos.push(_projectSlug);
     }
     // Update Repo
-    function updateRepo(string memory _projectSlug, string memory _repoHash) public{
+    function updateRepo(bytes15 _projectSlug, bytes memory _repoHash) public{
         userRepos[msg.sender][_projectSlug].repoHash =  _repoHash;
     }
    // Get Repo Name
-    function getRepoName(string memory _projectSlug) public view returns(string memory) {
+    function getRepoName(bytes15 _projectSlug) public view returns(bytes15) {
         return repos[_projectSlug].repoName;
     }
    // Get Repo Img
-    function getRepoImg(string memory _projectSlug) public view returns(string memory) {
+    function getRepoImg(bytes15 _projectSlug) public view returns(bytes memory) {
         return repos[_projectSlug].repoHash;
     }
    // Get Repo Name
-    function getRepoHash(string memory _projectSlug) public view returns(string memory) {
+    function getRepoHash(bytes15 _projectSlug) public view returns(bytes memory) {
         return repos[_projectSlug].imgHash;
     }
     // Get User function
-    function getUser() public view returns(string memory) {
+    function getUser() public view returns(bytes15) {
         return users[msg.sender].userName;
     }
     // Get All Users
@@ -58,7 +67,7 @@ contract Data {
         return allUsers;
     }
     // Get All Users
-    // function getAllRepos() public view returns(string[] memory) {
-    //     return allRepos;
-    // }
+    function getAllRepos() public view returns(bytes15[] memory) {
+        return allRepos;
+    }
 }
