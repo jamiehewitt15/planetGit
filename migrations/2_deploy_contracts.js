1aff75a449a515ef1510176472b9c88e7cb44e3c
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-10-24 13:51:38 +0300

    assigning ownership to repo contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,9 @@ const GLDToken = artifacts.require("GLDToken");
 
 module.exports = async function(deployer) {
   deployer.deploy(User);
-  deployer.deploy(Repo);
+  await deployer.deploy(Repo);
+  const repoAddress = await Repo.deployed();
+  console.log("repoAddress: ", repoAddress.address)
   const _initialSupply = 1000000000;
-  deployer.deploy(GLDToken, _initialSupply, '0x2d6edDF06b63F53DAa6Cd9727aD40eB90Dab8559');
+  deployer.deploy(GLDToken, _initialSupply, repoAddress.address);
 };
