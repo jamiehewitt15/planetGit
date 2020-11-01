873999c990ce94ba1d6f1c69c72589c75c15c0cc
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-11-02 00:27:36 +0300

    git clone updated with minting tokens

--- cli/src/clone.js
+++ cli/src/clone.js
@@ -1,7 +1,6 @@
 const shell = require('shelljs');
 const readlineSync = require('readline-sync');
 const argv = require('minimist')(process.argv.slice(2));
-let repoHash = '';
 // Assign arguments
 let newFolder;
 let projectSlug;
@@ -18,7 +17,7 @@ if (!argv._[1]){
   projectSlug = argv._[1]
 }
 
-const cloneRepo = async() => {
+const cloneRepo = async(repoHash) => {
   const folder = newFolder.replace(/(\r\n|\n|\r)/gm, "");
   repoHash = repoHash.replace(/(\r\n|\n|\r)/gm, "");
   console.log('Cloning into: ', 'http://127.0.0.1:8080/ipfs/' + repoHash, folder);
@@ -31,13 +30,14 @@ const cloneRepo = async() => {
     shell.exec(`git clone http://127.0.0.1:8080/ipfs/${repoHash} ${folder}`)
   }
   console.log("Finished");
+  shell.exit(0);
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const startClone = async() => {
+const startClone = async(repoHash) => {
   if (!shell.which('git')) { // check if user has git installed
     shell.echo('Sorry, this script requires git');
     shell.exit(1);
@@ -45,7 +45,7 @@ const startClone = async() => {
     if (shell.which('ipfs')) { // check if user has IPFS installed
       shell.exec(`ipfs daemon`, {async:true, silent:true});
       await sleep(2000); // wait for IPFS Daemon to start
-      await cloneRepo();
+      await cloneRepo(repoHash);
       }
       else{ // Use public IPFS node
       console.log("We recomend installing IPFS")
@@ -69,32 +69,76 @@ const provider = 'HTTP://127.0.0.1:7545' // Main-net: 'https://mainnet.infura.io
 const web3Provider = new Web3js.providers.WebsocketProvider(provider);
 const web3 = new Web3js(web3Provider);
 
-const { abi, networks } = require('../abis/Repository.json');
+// const { abi: repositoryAbi , networks: repositoryNetworks  } = require('../abis/Repository.json');
+const { abi: mintAbi , networks: mintNetworks  } = require('../abis/MintReward.json');
 
 let accountAddress = shell.cat("accountAddress.txt").stdout;
 let privateKey = '';
 
-const loadContractAddress = async () => {
+const loadContractAddress = async (networks) => {
     // Get smart contract network
     const networkId = await web3.eth.net.getId();
     // Get contract address
     return networks[networkId].address;
 }
-const loadContract = async (contractAddress) => {
+const loadContract = async (abi, contractAddress) => {
     const contract = web3.eth.Contract(abi, contractAddress);
     return contract;
 }
 
-async function setup(){
-    const contractAddress = await loadContractAddress();
-    const contract = await loadContract(contractAddress);
-    repoHash = await contract.methods.getRepoHash(projectSlug).call();
+const getHash = async () => {
+              // const repositoryAddress = await loadContractAddress(repositoryNetworks);
+              // const repositoryContract = await loadContract(repositoryAbi, repositoryAddress);
+              const mintAddress = await loadContractAddress(mintNetworks);
+              const mintContract = await loadContract(mintAbi, mintAddress);
+              const nonce = web3.utils.randomHex(4);
+              await mintReward(mintAddress, mintContract, projectSlug, nonce); // await mintContract.methods.mintReward(projectSlug, nonce);
+              await mintContract.events.Hash({
+                filter: {_from: accountAddress, nonce: nonce}, 
+                fromBlock: 0
+                })
+                .on("connected", function(subscriptionId){
+                    console.log(">>> subscriptionId: ", subscriptionId);
+                })
+                .on('data', async function(event){
+                    const returnHash = await event.returnValues._value;
+                    console.log("returnHash", returnHash)
+                    await startClone(returnHash);
+                    return returnHash;
+                })
+                .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+                    console.log(">>> Error: ", error);
+                });
+}
+
+async function mintReward(contractAddress, contract, projectSlug, nonce){
+  let privateKeyBuffer;
+  const privateKey = readlineSync.question('\n\nPlease enter you private key:\n\n');
+  try {
+      privateKeyBuffer = await Buffer.from(privateKey, 'hex');
+      console.log("privateKeyBuffer", privateKeyBuffer)
+  } catch (error) {
+      console.log(">>> error 1", error)
+  }
+  const count = await web3.eth.getTransactionCount(accountAddress);
+  //creating raw tranaction
+  const rawTransaction = await {
+    "from":accountAddress, 
+    "gasLimit":web3.utils.toHex(2100000),
+    "to":contractAddress,"value":"0x0",
+    "data":contract.methods.mintReward(projectSlug, nonce).encodeABI(), 
+    "nonce":web3.utils.toHex(count)
+    }
+    //creating tranaction via ethereumjs-tx
+    const transaction = await new EthereumTx(rawTransaction);
+    //signing transaction with private key
+    transaction.sign(privateKeyBuffer);
+    //sending transacton via web3 module
+    web3.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
 }
 
 async function run(){
-    await setup();
-    await startClone();
-    shell.exit(0);
+    await getHash();
 }
 
 run();
