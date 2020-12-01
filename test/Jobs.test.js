04ec78f42f01f844bb78fb1a54b466b3013a274f
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-12-01 18:08:21 +0300

    removing console logs

--- test/Jobs.test.js
+++ test/Jobs.test.js
@@ -38,18 +38,14 @@ contract('Jobs', (accounts)=>{
 
     describe('Functions', async () => {
         it('Creates a job', async () => {
-            const initialBalance = parseInt(await token.balanceOf(walletAddress));
             const title = "Jobtitle";
             const description = "Jobtitle - this is a fantastic job. Start today!"; 
             const monthly = false;
             const salary = 50;
             await token.approve(jobs.address, salary);
             await jobs.createJob(title, description, monthly, salary)
-            const finalBalance = parseInt(await token.balanceOf(walletAddress));
-            // assert.notEqual(finalBalance, initialBalance);
-            // assert.isAbove(initialBalance, finalBalance, 'Final balance is greater than middle balance');
-        
             const retunJob = await jobs.getJob(0);
+            
             assert.equal(retunJob.title, title);
             assert.equal(retunJob.description, description);
             assert.equal(retunJob.monthly, monthly);
